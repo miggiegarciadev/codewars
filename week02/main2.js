2304f49f6c1cf05f565064c2ba9c89fb7a4d400e
miggiegarciadev <miggiegarciadev@gmail.com>
2021-03-03 21:50:32 -0626

    daily codewars

--- week02/main2.js
+++ week02/main2.js
@@ -1,6 +1,24 @@
-// P (Parameters: whats  coming into the function [input])
-// R(Returns: what is the result, what do you end up wtih?)
-// E(Examples: play with the idea, write down things that
-//   can actually happen)
-// P(Psuedo Code: plain English what you want to happen, step by
-//   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+/*
+Link: https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+Instructions:
+Implement a function which multiplies two numbers.
+
+// Write here your multiply-function
+
+P 2 numbers which i identify as num1 and num2
+R the multiple of the two parameters
+E 2*3=6 or 4*4=16 or 5*1=5
+Psuedo Code
+make a function named multiply
+num1 and num2 are the params
+argument being pass the 2 numbers (num1 and num2)
+num1 and num2 because i neeed two numbers (idk what the two numbers equal)
+operand * for multiplication
+return the product num1 * num2
+
+*/
+function multiply(num1, num2){
+  answer = num1 * num2
+  return answer
+}
