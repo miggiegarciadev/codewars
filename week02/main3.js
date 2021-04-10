ecdd6c995be0c3b86999268bbda756f44ba13fe4
miggiegarciadev <miggiegarciadev@gmail.com>
2021-04-10 16:28:15 -0400

    daily codewars

--- week02/main3.js
+++ week02/main3.js
@@ -1,6 +1,35 @@
-// P (Parameters: whats  coming into the function [input])
-// R(Returns: what is the result, what do you end up wtih?)
-// E(Examples: play with the idea, write down things that
-//   can actually happen)
-// P(Psuedo Code: plain English what you want to happen, step by
-//   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+/*
+Link: https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+Instructions:
+Now you have to write a function that takes an argument and returns the square of it.
+
+P num
+R square of the arguement
+E 3 squared is 9 or 4 sqaured is 12 2 squared is 4
+Psuedo code
+make a function name square (name of function given below)
+pass in arguement num =>
+idk what the value of number that is being squared is
+quick math = num^2 = new number **
+assign a variable declaration to new number  example: let newNum = math library method
+show the new number
+**think about what method from the Math library you can use to square
+a number in JS
+
+Math.power(num1, num2)
+- num1 is the base number
+- num2 is the exponent which the base number is being raised to
+
+
+*/
+// Write the "square"-function here
+
+function square(num){
+  //function name was given in comments
+  //i declared the arguement to be num which is the number that needs to be sqaured
+  let newNum = Math.pow(num, 2)
+  //num is the base and 2 is the square root
+  return newNum
+  //this is returning what the number is sqaured after using the math library method of pow
+}
