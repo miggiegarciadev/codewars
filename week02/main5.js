6a335ee8556b467c61e0b000dd9683cab43ec0fa
Miggie Garcia(they/them/their) <60056056+miggiegarciadev@users.noreply.github.com>
2021-04-11 13:14:05 -0400

    updated file

--- week02/main5.js
+++ week02/main5.js
@@ -1,6 +1,22 @@
-// P (Parameters: whats  coming into the function [input])
-// R(Returns: what is the result, what do you end up wtih?)
-// E(Examples: play with the idea, write down things that
-//   can actually happen)
-// P(Psuedo Code: plain English what you want to happen, step by
-//   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+//var a == "code";
+//var b == "wa.rs";
+//var name == a + b;
+
+//Link: https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/javascript
+//Instructions: 
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+//the original problem had two equal signs and that is not how you assign a variable.
+// one equal sign is what is used to assigned a variable 
+//the log method helps me to do any debugging if anyyed 
+
+//P name 
+//R codewa.rs
+//E double equal versus single equal sign 
+//Psuedo Code 
+//pay attn to how code is written and do research if needed
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+// console.log(name)
+return name
