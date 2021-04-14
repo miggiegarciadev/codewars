49dd21bf96f353035009996a5be023147491ae86
Miggie Garcia(they/them/their) <60056056+miggiegarciadev@users.noreply.github.com>
2021-04-14 00:10:54 -0400

    Update main2.js

--- week03/main2.js
+++ week03/main2.js
@@ -1,6 +1,33 @@
-// P (Parameters: whats  coming into the function [input])
-// R(Returns: what is the result, what do you end up wtih?)
-// E(Examples: play with the idea, write down things that
-//   can actually happen)
-// P(Psuedo Code: plain English what you want to happen, step by
-//   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+/*
+Link: https://www.codewars.com/kata/57e92e91b63b6cbac20001e5/train/javascript
+
+Instructions: 
+
+P normPrice, discount, hol
+R an interger rounded down
+E 
+(12, 50, 1000), 166
+(17, 10, 500), 294
+(24, 35, 3000), 357
+Psuedo Code 
+1. find the savings
+2. round up to nearest whole interger
+3. return the value of step 2 
+
+high street price let normPrice =
+duty free discount let discount =
+cost of the holiday let hol
+all inputs are intergers and not decimals
+The Math.floor() function returns the largest integer less than or equal to a given number.
+*/
+
+function dutyFree(normPrice, discount, hol){
+  var savings = normPrice * discount / 100;
+  //step 1 above
+  return Math.floor(hol/savings)
+  //step 2 and 3 together
+  console.log(normPrice)
+  console.log(discount)
+  console.log(hol)
+  //testing to see what my examples are 
+}
