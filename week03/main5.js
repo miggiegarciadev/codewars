3daac76bb2aa74c8816157aba39a89e4829bba25
Miggie Garcia(they/them/their) <60056056+miggiegarciadev@users.noreply.github.com>
2021-04-15 00:55:44 -0400

    update main 5

--- week03/main5.js
+++ week03/main5.js
@@ -1,6 +1,46 @@
-// P (Parameters: whats  coming into the function [input])
-// R(Returns: what is the result, what do you end up wtih?)
-// E(Examples: play with the idea, write down things that
-//   can actually happen)
-// P(Psuedo Code: plain English what you want to happen, step by
-//   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+/*
+link: https://www.codewars.com/kata/51c89385ee245d7ddf000001/train/javascript
+
+instructions:
+Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+Params: value
+Return: "Value is 00005"
+Example:
+(solution(5),"Value is 00005")
+(solution(109),"Value is 00109")
+Psuedo Code:
+1. console.log to see the test case b/c instructions not clear
+2. number from solution gets put into a string
+3. new string returns value from solution and "Value is ..."
+4. set string1 to be first 1/2 of setence
+5. set string 2 to be all the zeros
+6. extra just the part i need from the parameter
+7. return new string
+
+ go from one string to another
+element 1 (string 1) becomes the last element in string 2
+
+test case
+expected '5this is the value' to equal 'Value is 00005'
+
+beginIndex
+The zero-based index at which to begin extraction. If negative, it is treated as str.length + beginIndex. (For example, if beginIndex is -3, it is treated as str.length - 3.) If beginIndex is not a number after Number(beginIndex), it is treated as 0.
+
+If beginIndex is greater than or equal to str.length, an empty string is returned.
+
+
+function solution(value){
+  //...
+
+}
+
+*/
+
+function solution(value){
+  var string1 = 'Value is '
+  var string2 = '0000'
+  return string1 + (string2 + value).slice(-5)
+  //.slice(-5) is basically saying value.length-5 in order to extra the part of the value that I need
+  //console.log("Value is " + ("00000" + value).slice(-5));
+}
