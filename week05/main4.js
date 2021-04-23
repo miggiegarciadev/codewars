fb7760bb0a5d8e0f81a85cba3ef9b10eec8ff38d
Miggie Garcia(they/them/their) <60056056+miggiegarciadev@users.noreply.github.com>
2021-04-23 09:17:50 -0400

    Update main4.js

--- week05/main4.js
+++ week05/main4.js
@@ -4,3 +4,23 @@
 //   can actually happen)
 // P(Psuedo Code: plain English what you want to happen, step by
 //   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+
+Century of the Year 8kyu 
+
+/* The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+
+Task :
+Given a year, return the century it is in.
+
+PREP 
+Parameters: century, year
+Results: 
+Examples:
+Psuedo Code:
+*/
+
+
+function century(year) {
+  // Finish this :)
+  return Math.ceil(year/100) // ceil rounds the whole number up 
+}
