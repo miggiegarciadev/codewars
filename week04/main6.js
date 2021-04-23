2fc88e2d01feb751eface8c9453597c3036d6473
Miggie Garcia(they/them/their) <60056056+miggiegarciadev@users.noreply.github.com>
2021-04-23 09:14:00 -0400

    Update main6.js

--- week04/main6.js
+++ week04/main6.js
@@ -4,3 +4,27 @@
 //   can actually happen)
 // P(Psuedo Code: plain English what you want to happen, step by
 //   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+
+Link:
+
+Holiday VI - Shark Pontoon 8kyu
+
+Instructions: 
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+    if (dolphin === true){
+    let newSharkSpeed = sharkSpeed/2
+      if (pontoonDistance/youSpeed < sharkDistance/newSharkSpeed){
+      return "Alive!"
+      }else{
+      return "Shark Bait!"
+      }
+    } else{
+      if ((pontoonDistance/youSpeed) < (sharkDistance/sharkSpeed)){
+      return "Alive!"
+      }else{
+      return "Shark Bait!"
+      }
+}
+
+}
