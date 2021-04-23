76583fc80ea1d22fe7fa2608251bbd7fd04c83f1
Miggie Garcia(they/them/their) <60056056+miggiegarciadev@users.noreply.github.com>
2021-04-23 10:39:21 -0400

    Update main1.js

--- week06/main1.js
+++ week06/main1.js
@@ -4,3 +4,21 @@
 //   can actually happen)
 // P(Psuedo Code: plain English what you want to happen, step by
 //   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+
+Array plus Array
+
+function arrayPlusArray(arr1, arr2) {
+  var newValue = 0
+  var newNum = 0 
+  
+  for(var i = 0; i < arr1.length; i++){
+  var newValue = newValue += arr1[i];   
+}
+  for(var j = 0; j <arr2.length; j++){
+    var newNum = newNum += arr2[j];
+  
+  }
+  console.log (newValue + newNum);
+    return (newValue + newNum);
+    console.log  (arrayPlusArray(arr1 + arr2));
+  }
