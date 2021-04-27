77c93afb8551e029c93672626874e8fd8f4768d3
miggiegarciadev <miggiegarciadev@gmail.com>
2021-04-27 09:07:37 -0400

    daily codewars

--- week15/main5.js
+++ week15/main5.js
@@ -6,11 +6,23 @@
 //   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
 /*
 link:
-
-INTRUCTIONS:
-
-P
-R
-E
-P
+P num
+R "It's hotter than the sun!! || "Help yourself to a honeycomb Yorkie for the glovebox."
+E 2000 is greater than 1000
+395 is less than 1000
+Psuedo Code
+number sqaured
+number bigger than 1000 return "its hotter ...""
+number less than 1000 return "helo yoursel..."
+because im using exponents math.powe makes the most sense to use
 */
+
+function apple(num){
+
+  if (Math.pow(num,2) > 1000) {
+    return "It's hotter than the sun!!";
+  } else {
+    return "Help youself to a honeycomb Yorkie for the glovebox.";
+
+  }
+}
