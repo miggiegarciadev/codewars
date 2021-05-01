ce92fcb3af1aca5ad0ab21f199ffa46dafc6e104
Miggie Garcia <miggiegarciadev@gmail.com>
2021-04-30 20:00:00 -0823

    daily codewars

--- week17/main1.js
+++ week17/main1.js
@@ -13,4 +13,4 @@ function lovefunc(flower1, flower2){
   }
 }
 
-https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript ... 
