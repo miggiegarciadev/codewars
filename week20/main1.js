16d4d710b807b075e6dccd0e49502a55dc84922b
Miggie Garcia <miggiegarciadev@gmail.com>
2021-05-23 20:00:00 -0823

    updated files

--- week20/main1.js
+++ week20/main1.js
@@ -4,3 +4,15 @@
 //   can actually happen)
 // P(Psuedo Code: plain English what you want to happen, step by
 //   step... think of these questions: 1. What could the user do? 2. What does the user see? 3. What does the user expect?)
+
+https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  console.log(distanceToPump, mpg, fuelLeft)
+
+  if (distanceToPump / mpg == fuelLeft || distanceToPump / mpg <= fuelLeft){
+    return true
+  } else {
+    return false
+  }
+};
